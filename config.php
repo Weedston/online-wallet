6d695793339bc081794e3a76caeff70772cf6df7
Weedston <wikly2003@gmail.com>
2025-03-29 09:19:01 +0000

    Sync local changes to remote

--- config.php
+++ config.php
@@ -3,4 +3,11 @@ define ('HOST', 'localhost');
 define ('USER', 'root');
 define ('PASS', '8A08d423');
 define ('DB', 'wallet_btc');
+
+$CONNECT = mysqli_connect(HOST, USER, PASS, DB);
+
+// Проверка соединения
+if (!$CONNECT) {
+    die("Ошибка подключения к БД: " . mysqli_connect_error());
+}
 ?>
