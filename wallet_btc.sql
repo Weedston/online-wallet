2e3291349065334965a5b60f09a3b7b541795f3c
Weedston <wikly2003@gmail.com>
2025-04-07 13:03:01 +0000

    Sync local changes to remote

--- wallet_btc.sql
+++ wallet_btc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost:3306
--- Время создания: Апр 06 2025 г., 12:37
+-- Время создания: Апр 07 2025 г., 13:02
 -- Версия сервера: 8.0.41-0ubuntu0.22.04.1
 -- Версия PHP: 8.1.2-1ubuntu2.21
 
@@ -49,10 +49,9 @@ CREATE TABLE `ads` (
 --
 
 INSERT INTO `ads` (`id`, `user_id`, `amount_btc`, `rate`, `payment_method`, `fiat_currency`, `trade_type`, `status`, `created_at`, `updated_at`, `comment`, `buyer_id`, `min_amount_btc`, `max_amount_btc`) VALUES
-(2, 184, '0.00030000', '7204926.00', 'Сбербанк', 'RUB', 'sell', 'pending', '2025-03-28 07:42:18', '2025-03-29 12:23:44', 'Готов', 182, 0, 0),
-(3, 182, '0.00030000', '7012641.00', 'Сбербанк', 'EUR', 'sell', 'pending', '2025-03-28 09:11:33', '2025-04-06 12:36:24', 'Только Сбер по номеру телефона', 184, 0.0004, 0.002),
-(4, 182, '0.00200000', '7150000.00', NULL, 'RUB', 'buy', 'active', '2025-03-28 15:05:04', '2025-04-06 12:36:46', '', 184, 0.00004, 0.003),
-(5, 182, NULL, '70187000.00', NULL, 'RUB', 'sell', 'active', '2025-04-06 11:33:23', '2025-04-06 11:33:23', NULL, NULL, 0.00002, 0.004);
+(6, 187, '0.00015000', '7006541.00', NULL, 'RUB', 'buy', 'pending', '2025-04-06 15:02:20', '2025-04-06 22:30:13', NULL, 182, 0.0001, 0.0002),
+(7, 184, NULL, '7001866.00', NULL, 'RUB', 'buy', 'active', '2025-04-06 15:22:11', '2025-04-06 15:22:11', '', NULL, 0.0002, 0.001),
+(8, 184, '0.00100000', '6998992.00', NULL, 'RUB', 'buy', 'active', '2025-04-06 15:34:52', '2025-04-06 16:32:50', '', NULL, 0.001, 0.002);
 
 -- --------------------------------------------------------
 
@@ -70,20 +69,14 @@ CREATE TABLE `ad_payment_methods` (
 --
 
 INSERT INTO `ad_payment_methods` (`ad_id`, `payment_method`) VALUES
-(2, 'OZON Банк'),
-(2, 'PayPal'),
-(4, 'PayPal'),
-(3, 'Wise '),
-(4, 'МИР'),
-(5, 'МИР'),
-(3, 'Сбербанк'),
-(5, 'Сбербанк'),
-(2, 'СБП'),
-(4, 'СБП'),
-(5, 'СБП'),
-(3, 'Совкомбанк'),
-(3, 'Т-Банк'),
-(5, 'Т-Банк');
+(6, 'СБП'),
+(7, 'СБП'),
+(8, 'СБП'),
+(6, 'Совкомбанк'),
+(7, 'Совкомбанк'),
+(6, 'Т-Банк'),
+(7, 'Т-Банк'),
+(8, 'Т-Банк');
 
 -- --------------------------------------------------------
 
@@ -102,8 +95,21 @@ CREATE TABLE `escrow_deposits` (
   `btc_amount` decimal(16,8) NOT NULL,
   `status` enum('waiting_deposit','btc_deposited','fiat_paid','btc_released','disputed','refunded') NOT NULL DEFAULT 'waiting_deposit',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `buyer_confirmed` tinyint(1) DEFAULT '0',
+  `seller_confirmed` tinyint(1) DEFAULT '0',
+  `arbiter_confirmed` tinyint(1) DEFAULT '0',
+  `buyer_cancelled` tinyint(1) DEFAULT '0',
+  `seller_cancelled` tinyint(1) DEFAULT '0',
+  `arbiter_cancelled` tinyint(1) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `escrow_deposits`
+--
+
+INSERT INTO `escrow_deposits` (`id`, `ad_id`, `escrow_address`, `buyer_pubkey`, `seller_pubkey`, `arbiter_pubkey`, `txid`, `btc_amount`, `status`, `created_at`, `updated_at`, `buyer_confirmed`, `seller_confirmed`, `arbiter_confirmed`, `buyer_cancelled`, `seller_cancelled`, `arbiter_cancelled`) VALUES
+(1, 6, '2MyK5oy7a2uNDdemoADVMCMg6V4phnfWu7s', '02260ff95657db6c3c1eeecf1231e8c1fe7e5c30a48530856aca16e19eda21c439', '0250e6bc9fe036fa94e88ce58f645b0cbc1920dd1b961040c72a53e4e8d8d836bc', '02260ff95657db6c3c1eeecf1231e8c1fe7e5c30a48530856aca16e19eda21c439', 'ac7bcfba0a790758875013551ad0d85226c3856cb754cb6ddcea564d856acf9a', '0.00015000', 'btc_deposited', '2025-04-06 22:30:13', '2025-04-06 22:30:13', 0, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -150,17 +156,19 @@ CREATE TABLE `members` (
   `passw` varchar(556) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `wallet` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `balance` varchar(256) DEFAULT NULL,
-  `username` varchar(255) NOT NULL,
-  `role` enum('admin','user') DEFAULT 'user'
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `role` enum('admin','user') DEFAULT 'user',
+  `pubkey` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Дамп данных таблицы `members`
 --
 
-INSERT INTO `members` (`id`, `passw`, `wallet`, `balance`, `username`, `role`) VALUES
-(182, 'motors whimpering titanate trumpet redeclares lobsters spouses combinator different magnificent recoil airfoils stammers Buchwald tentacled rarety-s rose-s murmured', 'tb1qtdxq5dzdv29tkw7t3d07qqeuz80y9k80ynu5tn', '0.01425463', '', 'user'),
-(184, 'chinquapin absentia missionaries milky pirate-s midband audiovisual continuities tableaux nowadays tamed protestant falsified Fredericksburg watchword directory-s uproots thermistor', 'tb1qfzxhvj6a6tf0cujun67wyr4m98q0danqftcl7x', '0.00000000', '', 'user');
+INSERT INTO `members` (`id`, `passw`, `wallet`, `balance`, `username`, `role`, `pubkey`) VALUES
+(182, 'motors whimpering titanate trumpet redeclares lobsters spouses combinator different magnificent recoil airfoils stammers Buchwald tentacled rarety-s rose-s murmured', 'tb1qtdxq5dzdv29tkw7t3d07qqeuz80y9k80ynu5tn', '0.00980495', '', 'user', '02260ff95657db6c3c1eeecf1231e8c1fe7e5c30a48530856aca16e19eda21c439'),
+(184, 'chinquapin absentia missionaries milky pirate-s midband audiovisual continuities tableaux nowadays tamed protestant falsified Fredericksburg watchword directory-s uproots thermistor', 'tb1qfzxhvj6a6tf0cujun67wyr4m98q0danqftcl7x', '0.00000000', '', 'user', '0387b9b086b1b947609b7fa03cd0da1db2fcef89f844756dc921f111a7b574451a'),
+(187, 'adjutants adjudged fondle lime condemner subinterval meadowland haltingly columnizes mild demonstrators upperclassman unnecessary stabile temerity grudge-s MD slanderer', 'tb1q075lflht5yztm87tj7kldkysfxw0y84jdhgjf7', '0.00425463', NULL, 'user', '0250e6bc9fe036fa94e88ce58f645b0cbc1920dd1b961040c72a53e4e8d8d836bc');
 
 -- --------------------------------------------------------
 
@@ -176,18 +184,6 @@ CREATE TABLE `messages` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `recipient_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Дамп данных таблицы `messages`
---
-
-INSERT INTO `messages` (`id`, `ad_id`, `user_id`, `message`, `created_at`, `recipient_id`) VALUES
-(87, 3, 184, 'Привет, готовы?', '2025-03-29 19:09:28', 182),
-(88, 3, 182, 'да, готов.', '2025-03-29 19:10:18', 184),
-(89, 2, 182, 'Добрый день', '2025-03-30 06:33:29', 184),
-(90, 2, 182, 'ага,', '2025-03-30 06:45:41', 184),
-(91, 2, 182, '1', '2025-03-30 06:46:22', 184),
-(92, 2, 182, '21', '2025-03-30 07:00:58', 184);
 
 -- --------------------------------------------------------
 
@@ -208,15 +204,8 @@ CREATE TABLE `notifications` (
 --
 
 INSERT INTO `notifications` (`id`, `user_id`, `message`, `is_read`, `created_at`) VALUES
-(4, 182, 'Новое сообщение в чате по объявлению #2', 1, '2025-03-29 18:22:58'),
-(5, 182, 'Новое сообщение в чате по объявлению #2', 1, '2025-03-29 18:25:04'),
-(6, 184, 'Новое сообщение в чате по объявлению #2', 1, '2025-03-29 18:35:34'),
-(7, 182, 'Новое сообщение в чате по объявлению #3', 1, '2025-03-29 19:09:29'),
-(8, 184, 'Новое сообщение в чате по объявлению #3', 1, '2025-03-29 19:10:19'),
-(9, 184, 'A new chat message based on the ad #2. Go to the <a href=\"p2p-trade_history\">Trade history</a> section and continue the transaction.', 1, '2025-03-30 06:33:29'),
-(10, 184, 'A new chat message based on the ad #2. Go to the <a href=\"p2p-trade_history\">Trade history</a> section to continue the transaction.', 1, '2025-03-30 06:45:42'),
-(11, 184, 'A new chat message based on the ad #2. Go to the <a href=\"p2p-trade_history\">Trade history</a> section to continue the transaction.', 1, '2025-03-30 06:46:22'),
-(12, 184, 'A new chat message based on the ad #2. Go to the <a href=\"p2p-trade_history\">Trade history</a> section to continue the transaction.', 1, '2025-03-30 07:00:58');
+(14, 184, 'Your ad #8 has been accepted and is in the pending status. Go to the \"Trade history\" section and continue the transaction.', 1, '2025-04-06 16:07:07'),
+(15, 187, 'Your ad #6 has been accepted and is in the pending status. Go to the \"Trade history\" section and continue the transaction.', 1, '2025-04-06 22:30:14');
 
 -- --------------------------------------------------------
 
@@ -304,7 +293,7 @@ CREATE TABLE `visit_counter` (
 --
 
 INSERT INTO `visit_counter` (`page`, `count`) VALUES
-('total', 5800);
+('total', 5807);
 
 --
 -- Индексы сохранённых таблиц
@@ -398,13 +387,13 @@ ALTER TABLE `visit_counter`
 -- AUTO_INCREMENT для таблицы `ads`
 --
 ALTER TABLE `ads`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT для таблицы `escrow_deposits`
 --
 ALTER TABLE `escrow_deposits`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT для таблицы `fiat_currencies`
@@ -422,7 +411,7 @@ ALTER TABLE `logs`
 -- AUTO_INCREMENT для таблицы `members`
 --
 ALTER TABLE `members`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=187;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=188;
 
 --
 -- AUTO_INCREMENT для таблицы `messages`
@@ -434,7 +423,7 @@ ALTER TABLE `messages`
 -- AUTO_INCREMENT для таблицы `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT для таблицы `p2p_offers`
