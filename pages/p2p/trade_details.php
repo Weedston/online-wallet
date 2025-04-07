b21d6420ee44b7d640827f4a9917f5ec4e100080
Weedston <wikly2003@gmail.com>
2025-04-07 17:58:01 +0000

    Sync local changes to remote

--- pages/p2p/trade_details.php
+++ pages/p2p/trade_details.php
@@ -317,7 +317,7 @@ $current_user_role = $is_buyer ? 'buyer' : ($is_seller ? 'seller' : '');
         loadMessages();
         setInterval(loadMessages, 5000);
 
-         function fetchUnreadNotificationCount() {
+          function fetchUnreadNotificationCount() {
         var xhr = new XMLHttpRequest();
         xhr.open('POST', '/src/jsonrpc.php', true);
         xhr.setRequestHeader('Content-Type', 'application/json');
