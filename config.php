66dff969c6e72646fb7a2abdf8b7d245185ed2fb
Weedston <wikly2003@gmail.com>
2025-05-12 13:01:01 +0000

    Sync local changes to remote

--- config.php
+++ config.php
@@ -74,6 +74,6 @@ function bitcoinRPC($method, $params = []) {
     return 'Unknown ERROR: ' . $response;
 }
 
-define('TELEGRAM_BOT_TOKEN', '7911312217:AAG-7PrL9_75b159550PM8boBpgc5zsJ4Qw');
-define('TELEGRAM_CHAT_ID', '1727320137');
+define('TELEGRAM_BOT_TOKEN', '1111:2222w');
+define('TELEGRAM_CHAT_ID', '3333333333');
 ?>
